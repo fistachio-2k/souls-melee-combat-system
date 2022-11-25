b7539626fe5ab2bea40de424a08ed6953721f568
DorGd <dor.gid00@gmail.com>
2022-11-25 12:05:05 +0200

    Addoing FremenCharacter class and BP

--- Source/VoiceProject/FremenCharacter.cpp
+++ Source/VoiceProject/FremenCharacter.cpp
@@ -3,11 +3,22 @@
 
 #include "FremenCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
 // Sets default values
 AFremenCharacter::AFremenCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	// Rotation is determent by the camera and not by the controller.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
 
 }
 
@@ -15,7 +26,6 @@ AFremenCharacter::AFremenCharacter()
 void AFremenCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -38,12 +48,33 @@ void AFremenCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 
 void AFremenCharacter::MoveForward(float AxisValue)
 {
-	AddMovementInput(GetActorForwardVector() * AxisValue);
+	if ((Controller != nullptr) && (AxisValue != 0.0f))
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, AxisValue);
+	}
+	// AddMovementInput(GetActorForwardVector() * AxisValue);
 }
 
 void AFremenCharacter::MoveRight(float AxisValue)
 {
-	AddMovementInput(GetActorRightVector() * AxisValue);
+	if ( (Controller != nullptr) && (AxisValue != 0.0f) )
+	{
+		// find out which way is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+	
+		// get right vector 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
+		AddMovementInput(Direction, AxisValue);
+	}
+	// AddMovementInput(GetActorRightVector() * AxisValue);
 }
 
 void AFremenCharacter::LookUp(float AxisValue)
