ae3a9a6cc90d0fda6d6443bedc298286b0f8a882
DorGd <dor.gid00@gmail.com>
2022-12-10 18:43:28 +0200

    create BaseWeapon class
    BaseWaepon inherit from Equippable interface

---
+++ Source/VoiceProject/Items/BaseWeapon.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseWeapon.h"
+#include "GameFramework/Character.h"
+
+// Sets default values
+ABaseWeapon::ABaseWeapon()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	USceneComponent* Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+	
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComponent->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void ABaseWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABaseWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseWeapon::OnEquipped()
+{
+	// IEquippable::Execute_OnEquipped(this);
+	AttachActor(SocketName);
+}
+
+void ABaseWeapon::OnUnequipped()
+{
+	// Super::OnUnequipped();
+}
+
+void ABaseWeapon::AttachActor(FName SocketNameToAttach)
+{
+	auto Character = Cast<ACharacter>(GetOwner());
+	if (Character == nullptr)
+	{
+		UE_LOG(LogTemp,
+			Warning,
+			TEXT("Can't attach %s, owner cast to character failed."),
+			*this->GetName());
+		
+		return;
+	}
+	
+	AttachToComponent(Character->GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, SocketNameToAttach);
+}
+
+UPrimitiveComponent* ABaseWeapon::GetItemMesh()
+{
+	return MeshComponent;
+}
+
