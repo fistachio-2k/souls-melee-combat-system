7632c40a5fe310d82411a057aa29e09b517bc4b8
DorGd <dor.gid00@gmail.com>
2023-01-14 16:48:39 +0200

    Create combat actor component

---
+++ Source/VoiceProject/Components/CombatComponent.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "VoiceProject/Items/BaseWeapon.h"
+#include "CombatComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class VOICEPROJECT_API UCombatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UCombatComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	ABaseWeapon* GetMainWeapon() const;
+	void SetMainWeapon(ABaseWeapon* NewWeapon);
+	bool IsCombatEnabled() const;
+	void SetCombatEnabled(bool IsCombatEnabled);
+
+private:
+	ABaseWeapon* MainWeapon;
+	bool bIsCombatEnabled;
+};
