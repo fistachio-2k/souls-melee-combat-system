77760cda5b7661f73ea7f56d3ea1f3310864e7d6
DorGd <dor.gid00@gmail.com>
2023-02-27 22:07:01 +0200

    create CollisionTraceComponent basic logic

---
+++ Source/VoiceProject/Components/CollisionTraceComponent.cpp
@@ -0,0 +1,78 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CollisionTraceComponent.h"
+
+
+// Sets default values for this component's properties
+UCollisionTraceComponent::UCollisionTraceComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UCollisionTraceComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UCollisionTraceComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                             FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	CollisionTrace();
+	// ...
+}
+
+void UCollisionTraceComponent::EnableCollision()
+{
+	ClearHitActors();
+	SetComponentTickEnabled(true);
+}
+
+void UCollisionTraceComponent::DisableCollision()
+{
+	SetComponentTickEnabled(false);
+}
+
+bool UCollisionTraceComponent::CollisionTrace()
+{
+	if (MeshComponent == nullptr)
+	{
+		return false;
+	}
+
+	const FVector Start = MeshComponent->GetSocketLocation(StartSocketName);
+	const FVector End = MeshComponent->GetSocketLocation(EndSocketName);
+
+	TArray<FHitResult> OutHits;
+	UKismetSystemLibrary::SphereTraceMultiForObjects(GetWorld(), Start, End, TraceRadius, CollisionObjectType, false, ActorsToIgnore, DebugType.GetValue(), OutHits, true);
+
+	if(!OutHits.IsEmpty())
+	{
+		for (FHitResult HitResult : OutHits)
+		{
+			AActor* HitActor = HitResult.GetActor();
+			if (!AlreadyHitActors.Contains(HitActor))
+			{
+				AlreadyHitActors.Emplace(HitActor);
+				OnHit.Execute(HitResult);
+			}
+		}
+	}
+	
+	return true;
+}
+
+void UCollisionTraceComponent::ClearHitActors()
+{
+	AlreadyHitActors.Empty();
+}
+
---
+++ Source/VoiceProject/Components/CollisionTraceComponent.h
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "CollisionTraceComponent.generated.h"
+
+DECLARE_DELEGATE_OneParam(FHitResultDelegate, FHitResult)
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class VOICEPROJECT_API UCollisionTraceComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UCollisionTraceComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	void EnableCollision();
+	void DisableCollision();
+	bool CollisionTrace();
+	void ClearHitActors();
+
+	UPROPERTY(EditAnywhere, Category="Initialization")
+	FName StartSocketName;
+
+	UPROPERTY(EditAnywhere, Category="Initialization")
+	FName EndSocketName;
+
+	UPROPERTY(EditAnywhere, Category="Initialization")
+	float TraceRadius;
+
+	UPROPERTY(EditAnywhere, Category="Initialization")
+	TArray<TEnumAsByte<EObjectTypeQuery>> CollisionObjectType;
+
+	UPROPERTY(EditAnywhere, Category="Initialization")
+	TArray<AActor*> ActorsToIgnore;
+
+	UPROPERTY(EditAnywhere, Category="Initialization")
+	TEnumAsByte<EDrawDebugTrace::Type> DebugType;
+	
+private:
+	UPROPERTY()
+	TArray<AActor*> AlreadyHitActors;
+
+	UPROPERTY()
+	UPrimitiveComponent* MeshComponent;
+
+	FHitResultDelegate OnHit;
+};
