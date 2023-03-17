79e161eada5ae1ea097e19c19e8210f6a9045db7
DorGd <dor.gid00@gmail.com>
2023-03-17 18:43:54 +0200

    create basic StateMachine class

---
+++ Source/VoiceProject/Utils/StateMachine.cpp
@@ -0,0 +1,27 @@
+﻿#include "StateMachine.h"
+
+#include "Microsoft/AllowMicrosoftPlatformTypes.h"
+
+template <typename E, typename T0>
+void StateMachine<E, T0>::SetState(E NewState)
+{
+	if (NewState != CurrentState)
+	{
+		CurrentState = NewState;
+	}
+
+	// StateMachine<EAssociativity> test = new StateMachine();
+}
+
+template <typename E, typename T0>
+void StateMachine<E, T0>::StateBegins()
+{
+	OnStateBegin.Broadcast(CurrentState);
+}
+
+template <typename E, typename T0>
+void StateMachine<E, T0>::StateEnd()
+{
+	OnStateEnd.Broadcast(CurrentState);
+}
+
---
+++ Source/VoiceProject/Utils/StateMachine.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <type_traits>
+
+template <typename E, typename = std::enable_if_t<std::is_enum_v<E>>>
+class StateMachine
+{
+	DECLARE_MULTICAST_DELEGATE_OneParam(StateModifyDelegate, E);
+
+	E CurrentState;
+	
+public:
+	StateMachine() = default;
+	explicit StateMachine(E CurrentState): CurrentState(CurrentState) {}
+	
+	E GetCurrentState() const { return CurrentState; }
+	void SetState(E NewState);
+
+	StateModifyDelegate OnStateBegin;
+	StateModifyDelegate OnStateEnd;
+
+private:
+	void StateBegins();
+	void StateEnd();
+};
